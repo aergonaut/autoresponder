1a1110678b36a357e230975b0f3896fd28ac0b93
Chris Fung <aergonaut@gmail.com>
2017-07-18 15:54:14 -0700

    adds config option to ignore issues based on author

--- README.md
+++ README.md
@@ -4,4 +4,18 @@ This is a GitHub Integration built with [probot](https://github.com/probot/probo
 
 ![](https://cloud.githubusercontent.com/assets/173/23834371/788d330e-0723-11e7-9e71-81d77c01267d.png)
 
+## Configuration
+
+A `.github/autoresponder.yml` file can be used to specify configuration options for the bot.
+
+```yml
+# Use the ignore key to specify when the bot should ignore a new issue
+ignore:
+  # Ignore issues from specific users
+  users:
+    - octocat
+  # Ignore issues from users who are collaborators
+  collaborators: true
+```
+
 See [docs/deploy.md](docs/deploy.md) if you would like to run your own instance of this plugin.
--- index.js
+++ index.js
@@ -1,5 +1,51 @@
+const yaml = require('js-yaml');
+
+async function getConfig(context) {
+  let config;
+  try {
+    const options = context.repo({path: '.github/autoresponder.yml'});
+    const res = await context.github.repos.getContent(options);
+    config = yaml.load(new Buffer(res.data.content, 'base64').toString()) || {};
+  } catch (err) {
+    config = {};
+  }
+  return config;
+}
+
+async function shouldSkipIssueFromSender(sender, context, config) {
+  if (typeof config.ignore !== 'undefined') {
+    // Ignore specific users
+    if (typeof config.ignore.users !== 'undefined') {
+      if (config.ignore.users.indexOf(sender) !== -1) {
+        return true;
+      }
+    }
+
+    // Ignore collaborators
+    if (
+      typeof config.ignore.collaborators !== 'undefined' &&
+      config.ignore.collaborators == true
+    ) {
+      const options = context.repo({username: sender});
+      const isCollaborator = await context.github.checkCollaborator(options);
+      if (isCollaborator) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
 module.exports = robot => {
   robot.on('issues.opened', async context => {
+    const config = getConfig(context);
+
+    const senderLogin = context.event.payload.sender.login;
+    if (shouldSkipIssueFromSender(senderLogin, context, config)) {
+      return;
+    }
+
     const options = context.repo({path: '.github/ISSUE_REPLY_TEMPLATE.md'});
     const res = await context.github.repos.getContent(options);
     const template = new Buffer(res.data.content, 'base64').toString();
